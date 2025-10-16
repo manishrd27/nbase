65da27c6b61cfede3791444c501282c0770b77c7
manishrd <22r01a0599@cmrithyderabad.edu.in>
2025-10-17 01:26:27 +0530

    tan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import prisma from "@/lib/db";
 const Page = async() => {
   const users = await prisma.user.findMany();
   return (
-    <div className=" min-w-screen flex items-center justify-center">
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
       
       {JSON.stringify(users)}
     </div>
